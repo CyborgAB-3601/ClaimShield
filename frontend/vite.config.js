2c8d0233ec462d97c0421b5de9f78afe8ce92717
CyborgAB-3601 <102860184+CyborgAB-3601@users.noreply.github.com>
2026-07-26 15:51:45 +0530

    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['pure-abundance-production-0330.up.railway.app']
+  }
 })
